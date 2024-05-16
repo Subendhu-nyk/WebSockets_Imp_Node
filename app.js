f20269dc508c1b9c18a6840c4aa4b63cdf0de6e6
Subendhu-nyk <subendhu.nyk@gmail.com>
2024-05-16 11:11:07 +0530

    recieving message from server to client

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const socket=new Server(httpServer,{
 })
 socket.on('connection',(socket)=>{
     console.log(socket);
+    // sending message from server to client by using emit method in socket,message is the event key
+    socket.emit("message","hello this is message")
 })
 
 httpServer.listen(3000,()=>{
