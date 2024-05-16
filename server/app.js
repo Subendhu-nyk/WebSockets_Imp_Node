90369f9fd66c569801e20ae37c1c28ab83039f0c
Subendhu-nyk <subendhu.nyk@gmail.com>
2024-05-16 20:48:49 +0530

    multiplayer initial setup using vite

--- server/app.js
+++ server/app.js
@@ -4,18 +4,14 @@ const {Server}=require('socket.io')
 const httpServer=createServer()
 const socket=new Server(httpServer,{
     cors:{
-        origin:'http://127.0.0.1:5500'
+        origin:'http://localhost:5173/'
     }
 })
 socket.on('connection',(socket)=>{
     console.log(socket);
     // sending message from server to client by using emit method in socket,message is the event key
-    socket.on('message',(data)=>{
-     console.log("server message>>",data)   
-    })
-    socket.emit("message","hello this is message")
 })
 
 httpServer.listen(3000,()=>{
-    // console.log("server is connected")
+    console.log("server is connected")
 })
